4f356370d04a6939decde27142ffdcae9b15f4cd
ervinanovlianti <ervinanov03@gmail.com>
2023-11-16 17:34:21 +0800

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KendaraanController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\PesananController;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,11 +16,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Auth
+Route::get('/', [AuthController::class, 'index']);
+Route::get('/register', [AuthController::class, 'registrasi']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/', function () {
-    return view('dashboard');
-});
-Route::get('/pesanan', function () {
-    return view('pesanan');
-});
-Route::get('/kendaraan', [KendaraanController::class, 'index']);
+
+Route::get('/kendaraan', [KendaraanController::class, 'index'])->name('kendaraan.index');
+Route::get('/kendaraan/create', [KendaraanController::class, 'create'])->name('kendaraan.create');
+Route::post('/kendaraan/create', [KendaraanController::class, 'store'])->name('kendaraan.store');
+Route::get('/pegawai', [PegawaiController::class, 'index'])->name('pegawai.index');
+Route::get('/pegawai/create', [PegawaiController::class, 'create'])->name('pegawai.create');
+Route::post('/pegawai/create', [PegawaiController::class, 'store'])->name('pegawai.store');
+Route::get('/pesanan', [PesananController::class, 'index'])->name('pesanan.index');
+Route::get('/pesanan/create', [PesananController::class, 'create'])->name('pesanan.create');
+Route::post('/pesanan/create', [PesananController::class, 'store'])->name('pesanan.store');
+// route persetujuan
+
+Route::get('/pesanan/persetujuan', [PesananController::class, 'action'])->name('pesanan.action');
+Route::get('/pesanan/persetujuan/{id}', [PesananController::class, 'persetujuan'])->name('pesanan.persetujuan');
